d300b2828891adb3b4bb7cb272c8f80e2276f11d
Olivier <olivier@ogasphere.com>
2021-07-13 15:57:10 +0200

    Affichage du terrain de jeu

--- game.js
+++ game.js
@@ -1,15 +1,41 @@
 var game = {
     init: function(){
+
+        game.model= model;
        
+        game.dysplayGrid()
+
     },
+    
     cellWidth: 50,
     tableau: document.querySelector('.terrain_de_jeu'),
+    
     types: {
         'x': 'classic',
         '*': 'wall',
         'o': 'burger',
         '-': 'goal'
     },
+
+    dysplayGrid: function() {
+        for (let ligne = 0; ligne < game.model.length; ligne++) {
+            for (let column = 0; column < game.model[ligne].length; column++) {
+                maCellule = game.model[ligne][column];
+                console.log(maCellule);
+                let unCarre = document.createElement('div');
+                unCarre.className = 'square';
+                // On ajoute des classes "coordonnées" sous le format x-0 et y-0 par exemple.
+                unCarre.classList.add(game.types[maCellule], 'x-'+column, 'y-'+ligne);
+                game.tableau.appendChild(unCarre);
+
+
+            }
+            
+        }
+
+    },
+
+
 };
 
 
